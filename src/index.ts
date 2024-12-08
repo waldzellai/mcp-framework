62873b9fb67d41cff0fdca647b4d1a0ff4d35e6b
Alex Andru <alex007d@gmail.com>
2024-12-09 00:06:54 +0100

    feat: add index

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { MCPServer, type MCPServerConfig } from "./core/MCPServer.js";
+export { BaseTool, BaseToolImplementation } from "./tools/BaseTool.js";
+export { ToolLoader, type ToolLoaderOptions } from "./core/toolLoader.js";
